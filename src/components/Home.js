6b792362169e35599e7f382dc6ec99f5b277e651
tdeslauriers <thomas.deslauriers@gmail.com>
2022-01-24 14:09:42 -0600

    replacing manual redux with reduxjs/toolkit

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <p>Welcome to des Lauriers world.</p>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/components/Login.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+import { login } from "../slices/auth";
+import { clearMessage } from "../slices/message";
+
+const Login = (props) => {
+  const [loading, setLoading] = useState(false);
+
+  const { isLoggedIn } = useSelector((state) => state.auth);
+  const { message } = useSelector((state) => state.message);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(clearMessage());
+  }, [dispatch]);
+
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  // Yup
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().required("Username required!"),
+    password: Yup.string().required("Password required!"),
+  });
+
+  const handleLogin = (formValue) => {
+    const { username, password } = formValue;
+    setLoading(true);
+
+    dispatch(login({ username, password }))
+      .unwrap()
+      .then(() => {
+        props.history.push("/profile");
+        window.location.reload();
+      })
+      .catch(() => {
+        setLoading(false);
+      });
+  };
+
+  if (isLoggedIn) {
+    return <Navigate to="/profile" />;
+  }
+
+  return (
+    <div>
+      <div>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={handleLogin}
+        >
+          <Form>
+            <div>
+              <label htmlFor="username">Username</label>
+              <Field name="username" type="text" className="form-control" />
+              <ErrorMessage name="username" component="div" className="alert" />
+            </div>
+            <div>
+              <label>Password</label>
+              <Field name="password" type="password" className="form-control" />
+              <ErrorMessage name="password" component="div" className="alert" />
+            </div>
+            <div>
+              <button type="submit" className="btn" disabled={loading}>
+                {loading && <span className="spinner"></span>}
+                <span>Login</span>
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+
+      {message && (
+        <div>
+          <div role="alert">{message}</div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default Login;
